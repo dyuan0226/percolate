e52a81e8e91f69a6db9842106623b617c6597664
dyuan0226 <33184700+dyuan0226@users.noreply.github.com>
2019-08-04 01:42:32 -0400

    added plot.board() method

--- R/board.R
+++ R/board.R
@@ -13,7 +13,7 @@ board <- function(mat=NULL, n=5, p=0.25){
     assert_that(is_valid(mat), msg="input mat must be a valid board")
     class(mat) <- c("board", "matrix")
     attr(mat, "n") <- nrow(mat)
-    attr(mat, "p") <- sum(mat != 0)/length(mat)
+    attr(mat, "p") <- sum(mat == 0)/length(mat)
     return(mat)
   } else {
     assert_that(is.numeric(n) && is.numeric(p), msg="n and p must be numerics")
@@ -27,4 +27,24 @@ board <- function(mat=NULL, n=5, p=0.25){
     attr(mat2, "p") <- p
     return(mat2)
   }
+}
+
+#' plot.board(), plotting function for valid boards
+#'
+#' @param x specifies the board to be plotted; must be a valid board object
+#'
+#' @return the plot of the board x
+plot.board <- function(x){
+  assert_that(is_valid(x), msg="x must be a valid board")
+  assert_that("board" %in% class(x))
+  n <- attr(x, "n")
+  board_df <- data.frame(row=as.vector(row(x)), col=as.vector(col(x)), val=as.vector(x))
+  board_df$val <- factor(board_df$val)
+  title <- paste(n, "x", n, " board", sep="")
+  ggplot(data=board_df, mapping=aes(x=row, y=col, fill=val)) + 
+    geom_tile() + 
+    labs(title=title) +
+    theme_void() +
+    theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + 
+    scale_fill_manual(values=c("0"="black", "1"="white", "2"="lightblue3"))
 }
